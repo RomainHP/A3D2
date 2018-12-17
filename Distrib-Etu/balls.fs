8c33bfb6b9e5560450fc324ad1300044f40f393d
Romain Charpentier <romain.charpentier10@gmail.com>
2018-12-17 12:30:44 +0100

    correction formule

--- Distrib-Etu/balls.fs
+++ Distrib-Etu/balls.fs
@@ -11,7 +11,7 @@ varying float vRadius;
 
 vec3 apply_lambert(vec3 li, vec3 kd, vec3 light, vec3 wi)
 {
-	float cosTheta = dot(light,wi);
+	float cosTheta = dot(light,wi)/(length(light),length(wi));
 	// formule de lambert
 	vec3 l0 = li * (kd/M_PI) * cosTheta;
 	return l0;
@@ -22,13 +22,17 @@ vec3 apply_lambert(vec3 li, vec3 kd, vec3 light, vec3 wi)
 // diapo 31 du cours
 float apply_cook_torrance(float ni, float sigma, vec3 wo, vec3 wi, vec3 normal)
 {
-	vec3 m = (wi+wo)/abs(wi+wo);
-	float c = dot(wi,normal);
+	vec3 m = (wi+wo)/abs(wi+wo);	// vecteur au milieu de wo et wi
+	float theta_m = acos(dot(wo,m)/(length(wo)*length(m)));	// angle entre wo et m
+	float c = abs(dot(wi,m));
 	float g2 = sqrt(ni*ni + c*c - 1.0);
+	// f = facteur de fresnel
 	float f = 0.5 * pow(g2-c,2.0)/pow(g2+c,2.0) * (1.0 + (pow(c*(g2+c)-1.0,2.0)/pow(c*(g2-c)+1.0,2.0)));
-	float d = ((sigma+2.0)/M_PI)*0.5*pow(dot((wi + wo),normal),sigma);
+	// d = distribution beckmann
+	float d = 1.0 / (M_PI*pow(theta_m,2.0)*pow(cos(theta_m),4.0)) * exp(- pow(tan(theta_m),2.0)/(2.0*pow(theta_m,2.0)));
+	// g = ombrage et masquage
 	float g = min(1.0, min((2.0*dot(normal,m)*dot(normal,wo))/dot(wo,m), (2.0*dot(normal,m)*dot(normal,wi))/dot(wi,m)));
-	float fs = (d*g*f) / (4.0 * dot(wi,normal) * dot(wo,normal));
+	float fs = (f*d*g) / (4.0 * abs(dot(wi,normal)) * abs(dot(wo,normal)));
 	return fs;
 }
 
@@ -59,8 +63,8 @@ void main(void)
 	vec3 light = normalize(vLightSource.xyz-point3D);
 
 	vec3 l0 = apply_lambert(lightIntensity, vColor.xyz, light, n);
-	float fs = apply_cook_torrance(ni, vColor.w, light, light, n);
+	vec3 fs = vec3(apply_cook_torrance(ni, vColor.w, point3D, light, n));
 
 	// couleur en fonction de l'intensite lumineuse
-	gl_FragColor = vec4(l0, 1.0);
+	gl_FragColor = vec4(l0+fs, 1.0);
 }
