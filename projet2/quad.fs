21efa9a5afe451606962ee9a974af679f0d56fb6
Romain Charpentier <romain.charpentier10@gmail.com>
2019-01-08 10:45:34 +0100

    ajout disque

--- projet2/quad.fs
+++ projet2/quad.fs
@@ -1,38 +1,80 @@
 precision mediump float;
 
 varying vec3 vOrigin;
-varying vec3 vRay;
+varying vec3 vDirection;
 
-float intersectionDisque(vec3 origin, vec3 direction, vec3 center, float radius)
+//----------------------------------------------------------------------//
+struct Ray
 {
-    vec3 originDirection = origin + direction;
-    float a = (originDirection.x) * (originDirection.x)
-            + (originDirection.y) * (originDirection.y)
-            + (originDirection.z) * (originDirection.z);
+  vec3 origin;
+  vec3 direction;
+};
 
-    float b = - 2.0 * originDirection.x * center.x
-            - 2.0 * originDirection.y * center.y
-            - 2.0 * originDirection.z * center.z;
+//----------------------------------------------------------------------//
+struct Sphere
+{
+  vec3 center;
+  float radius;
+};
 
-    float c = - radius*radius + center.x*center.x + center.y*center.y + center.z*center.z;
+//----------------------------------------------------------------------//
+vec3 pointIntersection(Ray ray, Sphere sphere)
+{
+    vec3 origin = ray.origin - sphere.center;   // pour avoir (x-xc),(y-yc),(z-zc)
+
+    float a = dot(ray.direction,ray.direction); // correspond a dir.x2 + dir.y2 + dir.z2
+
+    float b = 2.0 * dot(origin,ray.direction);
+
+    float c = dot(origin,origin) - sphere.radius*sphere.radius;
+
+    float discriminant = b*b - 4.0*a*c;
+
+    float pt = 0.0;
+    if (discriminant>0.0){
+        float pt1 = (-b-sqrt(discriminant)) / (2.0*a);
+        float pt2 = (-b+sqrt(discriminant)) / (2.0*a);
+        if (pt1<pt2) {
+            pt = pt1;
+        } else {
+            pt = pt2;
+        }
+    }else if (discriminant==0.0){
+        pt = (-b)/(2.0*a);
+    }
+    return ray.direction*pt+origin;
+}
+
+//----------------------------------------------------------------------//
+float intersectionDisque(Ray ray, Sphere sphere)
+{
+    vec3 origin = ray.origin - sphere.center;   // pour avoir (x-xc),(y-yc),(z-zc)
+
+    float a = dot(ray.direction,ray.direction); // correspond a dir.x2 + dir.y2 + dir.z2
+
+    float b = 2.0 * dot(origin,ray.direction);
+
+    float c = dot(origin,origin) - sphere.radius*sphere.radius;
+
     float discriminant = b*b - 4.0*a*c;
     return discriminant;
 }
 
+//----------------------------------------------------------------------//
 void main(void)
 {
-    if ((vRay.x <= 0.02 && vRay.x >= 0.0) || (vRay.z <= 0.02 && vRay.z >= 0.0)
-        || (vRay.z <= (-12.0+0.021) && vRay.z >= -12.0) || (vRay.z >= (12.0-0.021) && vRay.z <= 12.0)
-        || (vRay.x <= (-18.0+0.021) && vRay.x >= -18.0) || (vRay.x >= (18.0-0.021) && vRay.x <= 18.0)) {
+    Ray ray = Ray(vOrigin,normalize(vDirection));
+    Sphere sphere = Sphere(vec3(0.0,200.0,0.0),40.0);
+
+    if ((vDirection.x <= 0.021 && vDirection.x >= 0.0) || (vDirection.z <= 0.021 && vDirection.z >= 0.0)
+        || (vDirection.z <= (-12.0+0.02) && vDirection.z >= -12.0) || (vDirection.z >= (12.0-0.02) && vDirection.z <= 12.0)
+        || (vDirection.x <= (-18.0+0.02) && vDirection.x >= -18.0) || (vDirection.x >= (18.0-0.02) && vDirection.x <= 18.0)) {
         gl_FragColor = vec4(1.0);
     } else {
-        gl_FragColor = vec4((vRay.x+18.0)/36.0,0.0,(vRay.z+12.0)/24.0,1.0);
+        if (intersectionDisque(ray,sphere)>=0.0){
+            gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+        } else {
+            gl_FragColor = vec4((vDirection.x+18.0)/36.0,0.0,(vDirection.z+12.0)/24.0,1.0);
+        }
     }
-    // float radius = 10.0;
-    // vec3 center = vec3(0.0,60.0,0.0);
-    // if (intersectionDisque(vOrigin,vRay,center,radius)>=0.0){
-    //     gl_FragColor = vec4(1.0,0.0,0.0,1.0);
-    // } else {
-    //     gl_FragColor = vec4(1.0,1.0,1.0,1.0);
-    // }
 }
