435baa3cfd9afe52f93096216470e9b48ed44838
Romain Charpentier <romain.charpentier10@gmail.com>
2019-01-09 10:20:29 +0100

    structures

--- projet2/quad.fs
+++ projet2/quad.fs
@@ -3,13 +3,46 @@ precision mediump float;
 varying vec3 vOrigin;
 varying vec3 vDirection;
 
-#define M_PI    3.141592653589793
+#define M_PI            3.141592653589793
+#define NEAR            50.0
+
+#define NB_LIGHTS       1
+#define NB_SPHERES      1
+#define NB_PLANES       1
+
+#define NONE    0
+#define SPHERE  1
+#define PLANE   2
+
+//----------------------------------------------------------------------//
+struct Material
+{
+    vec3 kd;    // (entre 0 et 1, multiplie par -ks)
+    float ks;
+    float n;    // (>0)
+};
+
+//----------------------------------------------------------------------//
+struct Plane
+{
+    vec3 normal;
+    float scal;
+    Material material;
+};
+
+//----------------------------------------------------------------------//
+struct Sphere
+{
+  vec3 center;
+  float radius;
+  Material material;
+};
 
 //----------------------------------------------------------------------//
 struct Light
 {
-  vec3 source;
-  vec3 intensity;
+  vec3 position;
+  vec3 power;
 };
 
 //----------------------------------------------------------------------//
@@ -20,58 +53,41 @@ struct Ray
 };
 
 //----------------------------------------------------------------------//
-struct Sphere
+struct RenderInfo
 {
-  vec3 center;
-  float radius;
-  vec3 color;
+  vec3 point;
+  vec3 normal;
+  Material material;
 };
 
 //----------------------------------------------------------------------//
-vec3 pointIntersection(Ray ray, Sphere sphere)
+struct Scene
 {
-    vec3 origin = ray.origin - sphere.center;   // pour avoir (x-xc),(y-yc),(z-zc)
-
-    float a = dot(ray.direction,ray.direction); // correspond a dir.x2 + dir.y2 + dir.z2
-
-    float b = 2.0 * dot(origin,ray.direction);
-
-    float c = dot(origin,origin) - sphere.radius*sphere.radius;
-
-    float discriminant = b*b - 4.0*a*c;
-
-    float pt = 0.0;
-    if (discriminant>0.0){
-        float pt1 = (-b-sqrt(discriminant)) / (2.0*a);
-        float pt2 = (-b+sqrt(discriminant)) / (2.0*a);
-        if (pt1<pt2) {
-            pt = pt1;
-        } else {
-            pt = pt2;
-        }
-    }else if (discriminant==0.0){
-        pt = (-b)/(2.0*a);
-    }
-    return ray.direction*pt+origin;
-}
+  Light[NB_LIGHTS] lights;
+  Sphere[NB_SPHERES] spheres;
+  Plane[NB_PLANES] planes;
+};
 
 //----------------------------------------------------------------------//
-bool intersectionSphere(Ray ray, Sphere sphere, out vec3 point)
+float intersectionSphere(Ray ray, Sphere sphere)
 {
     vec3 origin = ray.origin - sphere.center;   // pour avoir (x-xc),(y-yc),(z-zc)
 
+    // on developpe la formule de la sphere en fonction de (O+dt)
     float a = dot(ray.direction,ray.direction); // correspond a dir.x2 + dir.y2 + dir.z2
 
     float b = 2.0 * dot(origin,ray.direction);
 
     float c = dot(origin,origin) - sphere.radius*sphere.radius;
 
+    // on resout ensuite l'equation du second degre en calculant le discriminant
     float discriminant = b*b - 4.0*a*c;
 
-    float t = 0.0;
+    float t = -1.0;  // temps ou le rayon intersecte la sphere
     if (discriminant>0.0){
         float t1 = (-b-sqrt(discriminant)) / (2.0*a);
         float t2 = (-b+sqrt(discriminant)) / (2.0*a);
+        // on garde le point d'intersection le plus proche (donc t minimal)
         if (t1<t2) {
             t = t1;
         } else {
@@ -80,50 +96,79 @@ bool intersectionSphere(Ray ray, Sphere sphere, out vec3 point)
     }else if (discriminant==0.0){
         t = (-b)/(2.0*a);
     }
+    
+    return t;
+}
 
-    point = ray.direction*t+origin;
+//----------------------------------------------------------------------//
+float intersectionPlane(Ray ray, Plane plane)
+{
+    // float dotRayPlane = dot(ray.direction,plane.normal);
+    // if (dotRayPlane==0.0){
+    //     return false;
+    // } else {
+    //     vec3 planeOrigin = plane.normal * ray.origin;
+    //     ray.t = ( - planeOrigin.x - planeOrigin.y - planeOrigin.z - plane.scal) / (ray.direction.x*plane.normal.x + ray.direction.y*plane.normal.y + ray.direction.z*plane.normal.z );    
+    //     return true;
+    // }
+    return -1.0;
+}
 
-    return discriminant>=0.0;
+//----------------------------------------------------------------------//
+void createFixedScene(out Scene scene)
+{
+    Material material = Material(vec3(1.0,0.0,1.0), 1.0, 3.0);
+
+    Light lights[NB_LIGHTS];
+    lights[0] = Light(vec3(-40.0,20.0,0.0), vec3(1.0,1.0,1.0));
+
+    Sphere spheres[NB_SPHERES];
+    spheres[0] = Sphere(vec3(0.0,200.0,0.0), 10.0, material);
+
+    Plane planes[NB_PLANES];
+    planes[0] = Plane(vec3(1.0,1000.0,0.0), 1.0, material); 
+
+    Scene scene;
+    scene.lights[0] = Light(vec3(-40.0,20.0,0.0), vec3(1.0,1.0,1.0));
+    scene.spheres[0] = Sphere(vec3(0.0,200.0,0.0), 10.0, material);
+    scene.planes[0] = Plane(vec3(1.0,1000.0,0.0), 1.0, material); 
 }
 
 //----------------------------------------------------------------------//
 void main(void)
 {
-    Light light = Light(vec3(-40.0,20.0,0.0), vec3(1.0,1.0,1.0));
-    Ray ray = Ray(vOrigin, normalize(vDirection));
-    Sphere sphere = Sphere(vec3(0.0,200.0,0.0), 10.0, vec3(1.0,0.0,0.0));
-    Sphere sphere2 = Sphere(vec3(-40.0,200.0,0.0), 10.0, vec3(1.0,0.0,0.0));
-    Sphere sphere3 = Sphere(vec3(20.0,180.0,0.0), 20.0, vec3(1.0,0.0,0.0));
+    //h = normalize(i+o);
+    //cos alpha = dot(n,h);
+    //phong = kd/M_PI + vec3(ks) * (n+8)/8*M_PI * pow(cos(alpha),n);
+    //Scene scene = createFixedScene();
+    //Ray ray = Ray(vOrigin, normalize(vDirection));
 
-    if ((vDirection.x <= 0.021 && vDirection.x >= 0.0) || (vDirection.z <= 0.021 && vDirection.z >= 0.0)
-        || (vDirection.z <= (-12.0+0.02) && vDirection.z >= -12.0) || (vDirection.z >= (12.0-0.02) && vDirection.z <= 12.0)
-        || (vDirection.x <= (-18.0+0.02) && vDirection.x >= -18.0) || (vDirection.x >= (18.0-0.02) && vDirection.x <= 18.0)) {
-        gl_FragColor = vec4(1.0);
-    } else {
-        vec3 point;
-        if (intersectionSphere(ray,sphere,point)){
-            vec3 N = normalize(point-sphere.center);
-            N.y = - N.y;
-            vec3 wi = normalize(light.source-point);
-	        float cosTi = max(0.0,dot(wi,N));	// angle entre i et N necessaire pour les brdfs
-            vec3 Lo = light.intensity * sphere.color/M_PI * cosTi;
-	        gl_FragColor = vec4(Lo,1.0);
-        } else if (intersectionSphere(ray,sphere2,point)){
-            vec3 N = normalize(point-sphere2.center);
-            N.y = - N.y;
-            vec3 wi = normalize(light.source-point);
-	        float cosTi = max(0.0,dot(wi,N));	// angle entre i et N necessaire pour les brdfs
-            vec3 Lo = light.intensity * sphere2.color/M_PI * cosTi;
-	        gl_FragColor = vec4(Lo,1.0);
-        } else if (intersectionSphere(ray,sphere3,point)){
-            vec3 N = normalize(point-sphere3.center);
-            N.y = - N.y;
-            vec3 wi = normalize(light.source-point);
-	        float cosTi = max(0.0,dot(wi,N));	// angle entre i et N necessaire pour les brdfs
-            vec3 Lo = light.intensity * sphere3.color/M_PI * cosTi;
-	        gl_FragColor = vec4(Lo,1.0);
-        } else {
-            gl_FragColor = vec4(0.0);
-        }
-    }
+    //float tmin = -1.0;
+    //int nearestObject = -1;
+    //int objectType = NONE;
+    // for (int i=0; i<NB_SPHERES; i++){
+    //     float t = intersectionSphere(ray, scene.spheres[i]);
+    //     if (t>=0.0){
+    //         if (tmin==-1.0 || t<tmin) {
+    //             objectType = SPHERE;
+    //             nearestObject = i;
+    //             tmin = t;
+    //         }
+    //     }
+    // }
+
+    //RenderInfo renderinfo;
+    //if (nearestObject>=0){
+        // vec3 point = ray.direction*ray.t + ray.origin;
+        // vec3 n = normalize(point-scene.spheres[nearestObject].center);
+        // n.y = -n.y;
+        // renderinfo = RenderInfo(point, n, scene.spheres[nearestObject].material);
+
+        // vec3 wi = normalize(light.position-renderinfo.point);
+        // float cosTi = max(0.0,dot(wi,renderinfo.normal));
+        // vec3 Lo = light.power * renderinfo.material.kd/M_PI * cosTi;
+        // gl_FragColor = vec4(Lo,1.0);
+    //} else {
+        gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+    //}
 }
